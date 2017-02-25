f8671952bd3c918bec8b9748c803528af5486d05
Eero Paavola <paavola.ee@gmail.com>
2017-02-25 20:52:19 +0200

    Async http request + basic error handling

---
+++ app/src/main/java/com/eero/stormy/AlertDialogFragment.java
@@ -0,0 +1,23 @@
+package com.eero.stormy;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+public class AlertDialogFragment extends DialogFragment{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_text)
+                .setPositiveButton(R.string.error_ok_button_text, null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
--- app/src/main/java/com/eero/stormy/MainActivity.java
+++ app/src/main/java/com/eero/stormy/MainActivity.java
@@ -2,12 +2,62 @@ package com.eero.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "883d36c70835e4c07c4a80c52b8fc8be";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+
+        String forecastUrl = "https://api.darksky.net/forecast/" + apiKey +
+                              "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    Log.v(TAG, response.body().string());
+                    if(!response.isSuccessful()) {
+                        alertUserAboutError();
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+            }
+        });
+
+        Log.d(TAG, "Main UI code is running");
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
